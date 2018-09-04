7834c2ca07e0d35ac54e01a47ba36a4189c4f77b
John Herman Witham <john.h.witham@gmail.com>
2018-09-05 01:04:09 +0300

    Hooked input component to index file & played w/ observers and computed properties

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,18 @@
+import { computed, observer } from '@ember/object';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
 
-    isDisabled: true
+    isDisabled: true,
 
+    emailAddress: '',
+  /*
+    actualEmailAddress: computed('emailAddress', function() {
+      console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
+    }),
+
+    emailAddressChanged: observer('emailAddress', function() {
+      console.log('observer is called', this.get('emailAddress'));
+    })
+  */
 });
