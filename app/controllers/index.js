e5120e8f7a9d7e3bf37eb13361d3ae9da3a221bc
John Herman Witham <john.h.witham@gmail.com>
2018-09-05 01:24:47 +0300

    Fixed mismatched quotes to back ticks bug

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -11,7 +11,7 @@ export default Controller.extend({
     actions: {
       saveInvitation() {
         alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
-        this.set('responseMessage', "Thank you! We've just saved your email address: ${this.get('emailAddress')}");
+        this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
         this.set('emailAddress', '')
       }
     }
