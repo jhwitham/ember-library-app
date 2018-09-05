2647aef1e30f0074c45549c530e4a45f0d1ea939
John Herman Witham <john.h.witham@gmail.com>
2018-09-05 19:05:58 +0300

    Deleted commented code from earlier example & git ignore

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -20,14 +20,4 @@ export default Controller.extend({
         this.set('emailAddress', '')
       }
     }
-
-  /*
-    actualEmailAddress: computed('emailAddress', function() {
-      console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
-    }),
-
-    emailAddressChanged: observer('emailAddress', function() {
-      console.log('observer is called', this.get('emailAddress'));
-    })
-  */
 });
