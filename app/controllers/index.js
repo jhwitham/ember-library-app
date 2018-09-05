06ac0c9907fe345f5ad27ac72f9ea02990666dae
John Herman Witham <john.h.witham@gmail.com>
2018-09-05 18:50:25 +0300

    Add code to save user email & delete alert

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,16 +3,19 @@ import { match, not } from '@ember/object/computed';
 
 export default Controller.extend({
 
+    headerMessage: 'Coming Soon!',
     emailAddress: '',
+    responseMessage: '',
 
     isValid: match('emailAddress', /^.+@.+\..+$/),
     isDisabled: not('isValid'),
 
-    headerMessage: 'Coming Soon!',
-
     actions: {
       saveInvitation() {
-        alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+        const email = this.get('emailAddress')
+        const newInvintation = this.store.createRecord('invintation', { email: email});
+        newInvintation.save();
+
         this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
         this.set('emailAddress', '')
       }
