cae665e18f5e0c1a3bd158b09556c2f97484a8e5
John Herman Witham <john.h.witham@gmail.com>
2018-09-06 21:13:31 +0300

    Rewrote newInvintation to use JS promises

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -13,11 +13,12 @@ export default Controller.extend({
     actions: {
       saveInvitation() {
         const email = this.get('emailAddress')
-        const newInvintation = this.store.createRecord('invitation', { email: email});
-        newInvintation.save();
+        const newInvintation = this.store.createRecord('invitation', { email });
 
-        this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-        this.set('emailAddress', '')
+        newInvintation.save().then(response => {
+          this.set('responseMessage', `Thank you! We've just saved your email address with the following id: ${response.get('id')}`);
+          this.set('emailAddress', '');
+        })
       }
     }
 });
