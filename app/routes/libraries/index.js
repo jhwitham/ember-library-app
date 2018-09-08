2b1196f74c897d9b5edb851d7b844171cdfe71d4
John Herman Witham <john.h.witham@gmail.com>
2018-09-08 16:59:45 +0300

    Add library Destroy action with confirmation

--- app/routes/libraries/index.js
+++ app/routes/libraries/index.js
@@ -4,5 +4,15 @@ export default Route.extend({
 
   model() {
     return this.store.findAll('library');
+  },
+
+  actions: {
+    deleteLibrary(library) {
+      let confirmation = confirm('Are you sure');
+
+      if (confirmation) {
+        library.destroyRecord();
+      }
+    }
   }
 });
