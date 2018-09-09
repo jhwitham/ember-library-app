bf0635a5626faac1acf093b3c6b54ccc406584ed
John Herman Witham <john.h.witham@gmail.com>
2018-09-09 17:05:16 +0300

    Fixed bug where app wasnt rendering

--- app/models/library.js
+++ app/models/library.js
@@ -4,7 +4,7 @@ import { notEmpty } from '@ember/object/computed';
 export default DS.Model.extend({
   name: DS.attr('string'),
   address: DS.attr('string'),
-  phone: DS.attr('string')
+  phone: DS.attr('string'),
 
   isValid: notEmpty('name')
 });
