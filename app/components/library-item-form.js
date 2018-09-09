0ab875883becb542585bfd9e2d1e278c55a5dd93
John Herman Witham <john.h.witham@gmail.com>
2018-09-09 17:14:50 +0300

    Add buttonClicked action

--- app/components/library-item-form.js
+++ app/components/library-item-form.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  buttonLabel: 'Save',
+
+  actions: {
+
+    buttonClicked(param) {
+      this.sendAction('action', param);
+    }
+  }
 });
